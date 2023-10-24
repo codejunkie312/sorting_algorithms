618ba3cb65bf479968a2b34686b76e8db6357bcd
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-10-25 00:40:18 +0200

    fixed betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,33 +10,33 @@
  */
 void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
 {
-    size_t i = left, j = mid, k = left;
-    
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(array + left, mid - left);
-    printf("[right]: ");
-    print_array(array + mid, right - mid);
+	size_t i = left, j = mid, k = left;
 
-    while (i < mid && j < right)
-    {
-        if (array[i] <= array[j])
-            temp[k++] = array[i++];
-        else
-            temp[k++] = array[j++];
-    }
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + left, mid - left);
+	printf("[right]: ");
+	print_array(array + mid, right - mid);
 
-    while (i < mid)
-        temp[k++] = array[i++];
+	while (i < mid && j < right)
+	{
+		if (array[i] <= array[j])
+			temp[k++] = array[i++];
+		else
+			temp[k++] = array[j++];
+	}
 
-    while (j < right)
-        temp[k++] = array[j++];
+	while (i < mid)
+		temp[k++] = array[i++];
 
-    for (i = left; i < right; i++)
-        array[i] = temp[i];
-    
-    printf("[Done]: ");
-    print_array(array + left, right - left);
+	while (j < right)
+		temp[k++] = array[j++];
+
+	for (i = left; i < right; i++)
+		array[i] = temp[i];
+
+	printf("[Done]: ");
+	print_array(array + left, right - left);
 }
 
 /**
@@ -49,14 +49,14 @@ void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
  */
 void merge_sort_helper(int *array, int *temp, size_t left, size_t right)
 {
-    size_t mid = left + (right - left) / 2;
+	size_t mid = left + (right - left) / 2;
 
-    if (right - left <= 1)
-        return;
+	if (right - left <= 1)
+		return;
 
-    merge_sort_helper(array, temp, left, mid);
-    merge_sort_helper(array, temp, mid, right);
-    merge(array, temp, left, mid, right);
+	merge_sort_helper(array, temp, left, mid);
+	merge_sort_helper(array, temp, mid, right);
+	merge(array, temp, left, mid, right);
 }
 
 /**
@@ -67,15 +67,15 @@ void merge_sort_helper(int *array, int *temp, size_t left, size_t right)
  */
 void merge_sort(int *array, size_t size)
 {
-    int *temp;
+	int *temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    temp = malloc(size * sizeof(int));
-    if (!temp)
-        return;
-    
-    merge_sort_helper(array, temp, 0, size);
-    free(temp);
+	temp = malloc(size * sizeof(int));
+	if (!temp)
+		return;
+
+	merge_sort_helper(array, temp, 0, size);
+	free(temp);
 }
